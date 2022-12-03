ef9ae900a9d332b23487a78253cd7aa682191747
MariaMaksymyak <mariamaksymyak@ukr.net>
2022-12-03 09:02:57 +0000

    Added JS

--- js/main.js
+++ js/main.js
@@ -31,3 +31,15 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+
+// Mobile Navigation Menu
+let menuButton = document.querySelector('.menu__btn');
+
+menuButton.onclick = function () {
+  mobileMenu();
+}
+function mobileMenu() {
+  let menuList = document.querySelector('.menu__list');
+  menuList.classList.toggle('menu__list--active');
+}
